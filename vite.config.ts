bb03782886036d1970269f81aec237fd51e3a78e
zainaleinstein1982 <88931669+zainaleinstein1982@users.noreply.github.com>
2026-07-25 09:28:03 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/asih-winarti-personal-intelligence/',
   plugins: [react()],
   resolve: {
     alias: {
